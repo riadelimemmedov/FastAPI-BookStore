2645fea2e92e1f78cb8f94dcd1096f0b0c664629
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-18 10:08:49 +0400

    Is created ReaderBooks table.Created Many-To-Many realtionship Books and Readers table,Also created create_reader,read_book,get_reader_books api endpoint

--- store/main.py
+++ store/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, Request
 # ?Third Party packages
 import databases  # Databases gives you simple asyncio support for a range of databases.
 import sqlalchemy  # A powerful and popular Object-Relational Mapping (ORM) library that supports multiple database backends, including PostgreSQL, MySQL, SQLite, and more.
+from sqlalchemy import select
 from decouple import config
 
 
@@ -35,6 +36,24 @@ books = sqlalchemy.Table(
     sqlalchemy.Column("title", sqlalchemy.String),
     sqlalchemy.Column("author", sqlalchemy.String),
     sqlalchemy.Column("pages", sqlalchemy.Integer),
+)
+
+# *Readers
+readers = sqlalchemy.Table(
+    "readers",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.INTEGER, primary_key=True),
+    sqlalchemy.Column("first_name", sqlalchemy.String),
+    sqlalchemy.Column("last_name", sqlalchemy.String),
+)
+
+# *Readers Books
+readers_books = sqlalchemy.Table(
+    "readers_books",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("book", sqlalchemy.ForeignKey("books.id"), nullable=False),
+    sqlalchemy.Column("readers", sqlalchemy.ForeignKey("readers.id"), nullable=False),
 )
 
 
@@ -69,7 +88,6 @@ async def shutdown():
 @app.get("/books/")
 async def get_all_books():
     query = books.select()
-    print("Query", query)
     return await database.fetch_all(query)
 
 
@@ -78,5 +96,31 @@ async def get_all_books():
 async def create_book(request: Request):
     data = await request.json()
     query = books.insert().values(**data)
-    last_record_id = await database.execute(query)
-    return {"last_record_id": last_record_id}
+    book_id = await database.execute(query)
+    return {"book_id": book_id}
+
+
+#!create_reader
+@app.post("/readers/")
+async def create_reader(request: Request):
+    data = await request.json()
+    query = readers.insert().values(**data)
+    reader_id = await database.execute(query)
+    return {"reader_id": reader_id}
+
+
+#!read_book
+@app.post("/read/")
+async def read_book(request: Request):
+    data = await request.json()
+    query = readers_books.insert().values(**data)
+    reader_book_id = await database.execute(query)
+    return {"readers_book_id": reader_book_id}
+
+
+#!get_reader_books
+@app.get("/reader/books/{reader_id}/")
+async def get_reader_books(request: Request, reader_id: int):
+    query = readers_books.select().where(readers_books.c.readers == reader_id)
+    reader_books = await database.fetch_all(query)
+    return reader_books
